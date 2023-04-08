f83f05415e60e58de791e565313b892219d0db38
mr_spicysauce <mr_spicysauce@yahoo.com>
2023-04-08 23:58:02 +0100

    More content to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -11,7 +11,48 @@ export default function ContactMePage() {
             <main className="main-container">
                 <div className="header-container">
                     {" "}
-                    <h2>Contact me!</h2>
+                    <h1>Contact Me!</h1>
+                </div>
+
+                <div className="paragraph-container">
+                    <p>(This is just a theoretical page)</p>
+                </div>
+
+                <div className="header-container">
+                    <img
+                        width={"70"}
+                        height={"70"}
+                        src="/emojis/GitHubLogo.png"
+                    ></img>
+                    <h1>GitHub Account</h1>
+                </div>
+
+                <div className="header-container">
+                    <img
+                        width={"80"}
+                        height={"60"}
+                        src="/emojis/DiscordLogo.png"
+                    ></img>
+                    <h1>Discord Server</h1>
+                </div>
+
+                <div className="header-container">
+                    <img
+                        width={"90"}
+                        height={"90"}
+                        src="/emojis/YouTubeLogo.png"
+                    ></img>
+                    <h1>YouTube</h1>
+                </div>
+
+                <div className="header-container">
+                    {" "}
+                    <h1>Personal Website</h1>
+                </div>
+
+                <div className="header-container">
+                    {" "}
+                    <h1>Email</h1>
                 </div>
             </main>
         </>
